4bb21bfae537e574fdde1780b74b5813e83f3ffe
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2021-10-21 17:15:44 +0300

    Changed pricing text to russian

--- components/pricing/index.js
+++ components/pricing/index.js
@@ -6,49 +6,70 @@ import styles from './Pricing.module.css';
 const Pricing = () => {
   return (
     <section className={`${styles.pricing} ${styles.upAndDown}`}>
-      <h1 className="heading">our pricing plans</h1>
+      <h1 className="heading">цены и услуги</h1>
 
       <div className={styles.boxContainer}>
         <div className={styles.box}>
-          <h3 className={styles.boxTitle}>weekly</h3>
+          <h3 className={styles.boxTitle}>недельный</h3>
           <div className={styles.boxPrice}>
-            <span>$</span>20
+            <span>530 </span>&#8372;
           </div>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem magnam rem harum,
-            nostrum facilis labore excepturi dignissimos? Voluptatibus facere beatae quam quasi!
-            Vero fugit molestias officiis quam, corrupti earum aliquid?
-          </p>
-          <Button title={'select more'} />
+          <ol>
+            <li>Консультация логопеда-дефектолога</li>
+            <li>Комплексное развитие</li>
+            <li>Рисование</li>
+            <li>Красивый почерк</li>
+            <li>Английский язык</li>
+          </ol>
+          <Button title="Узнать больше" />
         </div>
 
         <div className={styles.box}>
           <span className={styles.boxChoise}>
-            best <br /> choise
+            лучший <br /> выбор
           </span>
-          <h3 className={styles.boxTitle}>monthly</h3>
+          <h3 className={styles.boxTitle}>месячный</h3>
           <div className={styles.boxPrice}>
-            <span>$</span>50
+            <span>1325 </span>&#8372;
           </div>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem magnam rem harum,
-            nostrum facilis labore excepturi dignissimos? Voluptatibus facere beatae quam quasi!
-            Vero fugit molestias officiis quam, corrupti earum aliquid?
-          </p>
-          <Button title={'select more'} />
+          <ol>
+            <li>Консультация логопеда-дефектолога</li>
+            <li>Комплексное развитие: первые 5 уроков</li>
+            <li>Рисование</li>
+            <li>Красивый почерк</li>
+            <li>Английский язык</li>
+            <li>Футбол</li>
+            <li>Театральная студия</li>
+            <li>Вокал</li>
+            <li>Робототехника</li>
+            <li>Подготовка к школе</li>
+          </ol>
+          <Button title="Узнать больше" />
         </div>
 
         <div className={styles.box}>
-          <h3 className={styles.boxTitle}>yearly</h3>
+          <h3 className={styles.boxTitle}>годовой</h3>
           <div className={styles.boxPrice}>
-            <span>$</span>250
+            <span>6625 </span>&#8372;
           </div>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem magnam rem harum,
-            nostrum facilis labore excepturi dignissimos? Voluptatibus facere beatae quam quasi!
-            Vero fugit molestias officiis quam, corrupti earum aliquid?
-          </p>
-          <Button title={'select more'} />
+          <ol>
+            <li>Консультация логопеда-дефектолога</li>
+            <li>Комплексное развитие: первые 5 уроков</li>
+            <li>Рисование</li>
+            <li>Красивый почерк</li>
+            <li>Английский язык</li>
+            <li>Футбол</li>
+            <li>Театральная студия</li>
+            <li>Вокал</li>
+            <li>Робототехника</li>
+            <li>Подготовка к школе</li>
+            <li>Шахматный клуб</li>
+            <li>Фитнес с элементами ритмики и ЛФК</li>
+            <li>Вокальный ансамбль</li>
+            <li>Гитара</li>
+            <li>Индивидуальные занятия со специалистом</li>
+          </ol>
+          <Button title="Узнать больше" />
         </div>
       </div>
     </section>
