7537d50be2b89db1fb93ba6267ca3dfb229a27f5
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2021-10-21 15:19:31 +0300

    Changed services text to russian

--- components/services/index.js
+++ components/services/index.js
@@ -12,48 +12,48 @@ import styles from './Services.module.css';
 const Services = () => {
   return (
     <section className={`${styles.services} ${styles.upAndDown}`}>
-      <h1 className="heading"> what we teach your childrens </h1>
+      <h1 className="heading">чему мы учим наших детей</h1>
 
       <div className={styles.boxContainer}>
         <div className={styles.box}>
           <Image src={pic1} alt="picture one" />
           <div className={styles.info}>
-            <h3 className={styles.boxTitle}>words and letters teaching</h3>
+            <h3 className={styles.boxTitle}>буквам и словам</h3>
           </div>
         </div>
 
         <div className={styles.box}>
           <Image src={pic2} alt="picture two" />
           <div className={styles.info}>
-            <h3 className={styles.boxTitle}>clay and crafting</h3>
+            <h3 className={styles.boxTitle}>лепить и мастерить</h3>
           </div>
         </div>
 
         <div className={styles.box}>
           <Image src={pic3} alt="picture three" />
           <div className={styles.info}>
-            <h3 className={styles.boxTitle}>drawing classNamees</h3>
+            <h3 className={styles.boxTitle}>рисовать и писать</h3>
           </div>
         </div>
 
         <div className={styles.box}>
           <Image src={pic4} alt="picture four" />
           <div className={styles.info}>
-            <h3 className={styles.boxTitle}>english classNamees</h3>
+            <h3 className={styles.boxTitle}>учить английский язык</h3>
           </div>
         </div>
 
         <div className={styles.box}>
           <Image src={pic5} alt="picture five" />
           <div className={styles.info}>
-            <h3 className={styles.boxTitle}>playground for childrens</h3>
+            <h3 className={styles.boxTitle}>знакомиться с детьми</h3>
           </div>
         </div>
 
         <div className={styles.box}>
           <Image src={pic6} alt="picture six" />
           <div className={styles.info}>
-            <h3 className={styles.boxTitle}>sports events</h3>
+            <h3 className={styles.boxTitle}>играть в игры</h3>
           </div>
         </div>
       </div>
