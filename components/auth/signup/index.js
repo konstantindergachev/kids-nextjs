f9c5cd3cca44b07ed043e46dd3cae28b4b500517
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2021-10-25 10:43:00 +0300

    Created the tales page route

--- components/auth/signup/index.js
+++ components/auth/signup/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 import Button from '@/shared/button';
 import Error from '@/shared/error';
 import { notifyService } from '@/services';
@@ -8,6 +9,8 @@ import styles from './Signup.module.css';
 const Signup = ({ onClose }) => {
   const [user, setUser] = useState({ firstname: '', lastname: '', email: '', password: '' });
   const [error, setError] = useState('');
+  const { push } = useRouter();
+
   const handleChange = (ev) => {
     setUser((oldState) => ({ ...oldState, [ev.target.name]: ev.target.value }));
   };
@@ -15,6 +18,7 @@ const Signup = ({ onClose }) => {
     ev.preventDefault();
     try {
       await notifyService.create({ endpoint: 'users', content: user });
+      push('/tales');
     } catch (error) {
       setError(error?.data?.message);
     }
---
+++ pages/tales/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './Tales.module.css';
+
+const Tales = () => {
+  return (
+    <section className={styles.test}>
+      <h1>Tales page</h1>
+    </section>
+  );
+};
+
+export default Tales;
