14326a88ec7f15f58ecc954c396efa6b256249a1
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2021-10-26 10:00:15 +0300

    Logged in user from the login form

--- components/auth/signin/index.js
+++ components/auth/signin/index.js
@@ -1,20 +1,54 @@
 import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 import Button from '@/shared/button';
+import Error from '@/shared/error';
+import { notifyService } from '@/services';
 
 import styles from './Signin.module.css';
 
 const Signin = ({ onClose }) => {
+  const [user, setUser] = useState({ email: '', password: '' });
+  const [error, setError] = useState('');
+  const router = useRouter();
+
+  const handleChange = (ev) => {
+    setUser((oldState) => ({ ...oldState, [ev.target.name]: ev.target.value }));
+  };
+  const handleSubmit = async (ev) => {
+    ev.preventDefault();
+    try {
+      const response = await notifyService.create({ endpoint: 'users/login', content: user });
+      const username = response?.user ? `${response.user.firstname} ${response.user.lastname}` : '';
+      router.push({ pathname: '/tales', query: { username } });
+    } catch (error) {
+      setError(error?.data?.message || error?.data?.errors.message);
+    }
+  };
+
   return (
     <div className={styles.login}>
       <h3>вход</h3>
-      <form action="">
-        <input type="email" placeholder="ваш email" />
-        <input type="password" placeholder="ваш пароль" />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          name="email"
+          placeholder="ваш email"
+          value={user.email}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="ваш пароль"
+          value={user.password}
+          onChange={handleChange}
+        />
+        {error && <Error message={error} />}
+        <div className={styles.buttonWrap}>
+          <Button title="отмена" onClick={onClose} />
+          <Button type="submit" title="войти" />
+        </div>
       </form>
-      <div className={styles.buttonWrap}>
-        <Button title="отмена" onClick={onClose} />
-        <Button title="войти" />
-      </div>
     </div>
   );
 };
