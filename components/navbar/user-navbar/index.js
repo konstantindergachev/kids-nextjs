f37a453bac70ef7582aa56ab1d72ef5eef6285cf
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2021-10-26 11:44:40 +0300

    Added the menu item

--- components/navbar/user-navbar/index.js
+++ components/navbar/user-navbar/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Button from '@/shared/button';
+import CustomLink from '@/components/shared/link';
 
 const UserNavbar = ({ username }) => {
   return (
     <>
+      <CustomLink pathname={`/tales?username=${username}`} title="сказки" />
       <Button title="выйти" />
       <span>{username}</span>
     </>
